54eb85c2d45ab7b3e14fff23a5733d6ffbe89339
Tyler Rick <tyler@k3integrations.com>
2011-01-19 16:08:18 -0800

    Changed :path to 'news' so that our URLs look like /news/my-title instead of /k3_blog_blog_post/my-title

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :k3_blog_blog_posts, :controller => 'k3/blog/blog_posts' do
+  resources :k3_blog_blog_posts, :path => 'news', :controller => 'k3/blog/blog_posts' do
     member do
       get :last_saved_status
     end
