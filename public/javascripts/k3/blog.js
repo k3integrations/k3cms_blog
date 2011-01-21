68cb4985e0b2e26df9f0de8824cf8b2b10028f97
Tyler Rick <tyler@k3integrations.com>
2011-01-21 11:45:39 -0800

    Added date to show and metadata_drawer

--- public/javascripts/k3/blog.js
+++ public/javascripts/k3/blog.js
@@ -5,6 +5,9 @@ k3_blog_blog_post = {
   updatePage: function(object_name, object_id, object) {
     K3_InlineEditor.updatePageFromObject(object_name, object_id, object)
 
+    // TODO: render_cell('k3/blog/blog_posts', :published_status, :blog_post => blog_post)
+    // console.log(object.date)
+
     // TODO: only if page title was originally set to @page.title. Perhaps we should set some JS variable to indicate which object/attribute the page title was taken form?
     $('title').html(object.title)
     $('meta[name=description]').attr('content', object.meta_description);
