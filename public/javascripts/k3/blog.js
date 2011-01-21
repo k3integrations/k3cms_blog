a331b8043c16bf50931fc482fd7aa94cf11fc9de
Tyler Rick <tyler@k3integrations.com>
2011-01-20 16:14:26 -0800

    Added meta_description and meta_keywords to blog_posts table and metadata_drawer.

--- public/javascripts/k3/blog.js
+++ public/javascripts/k3/blog.js
@@ -4,5 +4,10 @@ K3_Blog = {
 k3_blog_blog_post = {
   updatePage: function(object_name, object_id, object) {
     K3_InlineEditor.updatePageFromObject(object_name, object_id, object)
+
+    // TODO: only if page title was originally set to @page.title. Perhaps we should set some JS variable to indicate which object/attribute the page title was taken form?
+    $('title').html(object.title)
+    $('meta[name=description]').attr('content', object.meta_description);
+    $('meta[name=keywords]').   attr('content', object.meta_keywords);
   }
 }
